044737f65bf55b7d760cc693ed47152a6fc98980
Alena Bauer <alena.a.bauer@gmail.com>
2021-06-14 00:51:07 +0200

    reconnect server and react in index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Have Node serve the files for our built React app
-app.use(express.static('client/public'));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(cors());
 
 // Handle GET requests to /api route
@@ -48,6 +48,9 @@ app.get("/api/questions", (req, res) => {
 })
 
 // All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
