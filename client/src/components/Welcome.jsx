5fce6d7364595f324b72046932fccbbf79a5bb5f
Alena Bauer <alena.a.bauer@gmail.com>
2021-06-13 10:13:59 +0200

    change animation for the welcome heading

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 function Welcome() {
     return (
         <div className="welcome-container">
-            <h1 className="greeting"><span class="greeting-first-line">Дом Китайского Языка</span><span className="greeting-mid-line">представляет</span><span className="greeting-second-line">"своя игра"</span></h1>
+            <h1 className="greeting"><span class="greeting-first-line">Дом Китайского Языка</span><span className="greeting-mid-line">представляет</span><span className="greeting-second-line">своя игра</span></h1>
             <Link className="btn-start" to="/play">让我们玩吧！</Link>
         </div>
     )
