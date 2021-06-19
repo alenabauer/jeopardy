8421455ad89285980a9806f13c94ae76dc0c38a3
Alena Bauer <alena.a.bauer@gmail.com>
2021-06-19 13:48:39 +0200

    add all questions

--- client/src/components/Play.jsx
+++ client/src/components/Play.jsx
@@ -1,49 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, withRouter } from "react-router-dom";
+import LinkButton from './LinkButton';
 
 function Play() {
+
+    const [clicked, setAsClicked] = useState(false);
+
+    function handleClick(event) {
+        event.preventDefault();
+        setAsClicked(true);
+    }
 
     return(
     <div class="tablo">
         <div class="category-container">
             <div class="category-box"><p>Китай</p></div>
-            <Link class="score-box" to="/question/1">100</Link>
-            <Link class="score-box" to="/question/2">200</Link>
-            <Link class="score-box" to="/question/3">300</Link>
-            <Link class="score-box" to="/question/4">400</Link>
-            <Link class="score-box" to="/question/5">500</Link>
+            <LinkButton class={clicked ? "score-box clicked" : "score-box"} to="/question/1" onClick={handleClick}>100</LinkButton>
+            <LinkButton class="score-box" to="/question/2">200</LinkButton>
+            <LinkButton class="score-box" to="/question/3">300</LinkButton>
+            <LinkButton class="score-box" to="/question/4">400</LinkButton>
+            <LinkButton class="score-box" to="/question/5">500</LinkButton>
         </div>
         <div class="category-container">
             <div class="category-box"><p>Праздники</p></div>
-            <Link to="/question/6" class="score-box">100</Link>
-            <Link to="/question/7" class="score-box">200</Link>
-            <Link to="/question/8" class="score-box">300</Link>
-            <Link to="/question/9" class="score-box">400</Link>
-            <Link to="/question/10" class="score-box">500</Link>
+            <LinkButton to="/question/6" class="score-box">100</LinkButton>
+            <LinkButton to="/question/7" class="score-box">200</LinkButton>
+            <LinkButton to="/question/8" class="score-box">300</LinkButton>
+            <LinkButton to="/question/9" class="score-box">400</LinkButton>
+            <LinkButton to="/question/10" class="score-box">500</LinkButton>
         </div>
         <div class="category-container">
             <div class="category-box"><p>Чай</p></div>
-            <Link to="/question/11" class="score-box">100</Link>
-            <Link to="/question/12" class="score-box">200</Link>
-            <Link to="/question/13" class="score-box">300</Link>
-            <Link to="/question/14" class="score-box">400</Link>
-            <Link to="/question/15" class="score-box">500</Link>
+            <LinkButton to="/question/11" class="score-box">100</LinkButton>
+            <LinkButton to="/question/12" class="score-box">200</LinkButton>
+            <LinkButton to="/question/13" class="score-box">300</LinkButton>
+            <LinkButton to="/question/14" class="score-box">400</LinkButton>
+            <LinkButton to="/question/15" class="score-box">500</LinkButton>
         </div>
         <div class="category-container">
-            <div class="category-box"><p>Иероглифы</p></div>
-            <Link to="/question/16" class="score-box">100</Link>
-            <Link to="/question/17" class="score-box">200</Link>
-            <Link to="/question/18" class="score-box">300</Link>
-            <Link to="/question/19" class="score-box">400</Link>
-            <Link to="/question/20" class="score-box">500</Link>
+            <div class="category-box"><p>Слова</p></div>
+            <LinkButton to="/question/16" class="score-box">100</LinkButton>
+            <LinkButton to="/question/17" class="score-box">200</LinkButton>
+            <LinkButton to="/question/18" class="score-box">300</LinkButton>
+            <LinkButton to="/question/19" class="score-box">400</LinkButton>
+            <LinkButton to="/question/20" class="score-box">500</LinkButton>
         </div>
         <div class="category-container">
             <div class="category-box"><p>Еда</p></div>
-            <Link to="/question/21" class="score-box">100</Link>
-            <Link to="/question/22" class="score-box">200</Link>
-            <Link to="/question/23" class="score-box">300</Link>
-            <Link to="/question/24" class="score-box">400</Link>
-            <Link to="/question/25" class="score-box">500</Link>
+            <LinkButton to="/question/21" class="score-box">100</LinkButton>
+            <LinkButton to="/question/22" class="score-box">200</LinkButton>
+            <LinkButton to="/question/23" class="score-box">300</LinkButton>
+            <LinkButton to="/question/24" class="score-box">400</LinkButton>
+            <LinkButton to="/question/25" class="score-box">500</LinkButton>
         </div>
     </div>)
 };
