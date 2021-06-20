5290f604f470b561c2176a29e6b12ff77f506ede
Alena Bauer <alena.a.bauer@gmail.com>
2021-06-20 09:06:41 +0200

    change the show-answer-btn

--- client/src/components/FinalQuestion.jsx
+++ client/src/components/FinalQuestion.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, withRouter, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMedal } from '@fortawesome/free-solid-svg-icons';
+import { faMedal, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 function FinalQuestion(props) {
 
@@ -14,11 +14,13 @@ function FinalQuestion(props) {
 
     return (
         <div className="question-container">
-            <Link className="back-btn" to="/thankyou"><FontAwesomeIcon className="back-btn-icon thank-you" icon={faMedal} /></Link>
+            <div className="btn-container">
+                <Link className="back-btn first-btn" to="/thankyou"><FontAwesomeIcon className="back-btn-icon" icon={faMedal} /></Link>
+                <FontAwesomeIcon onClick={handleClick} className="back-btn-icon second-btn" icon={faQuestionCircle} />
+            </div>
             <p className={clicked ? "question-move" : "question"}> {props.question(25)} </p>
             {props.img(25) != undefined && <img class={clicked ? "question-img-move" : "question-img"} src={`../imgs/${props.img(25)}.jpg`} alt="question-img" />}
             {clicked && <div><p className="answer"> {props.answer(25)} </p></div>}
-            <button class="show-answer-btn" onClick={handleClick}>Ответ</button>
         </div>
     )
 }
--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, withRouter, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleLeft, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Question(props) {
 
@@ -18,11 +18,13 @@ function Question(props) {
 
     return (
         <div className="question-container">
-            <Link className="back-btn" to="/play"><FontAwesomeIcon className="back-btn-icon" icon={faArrowAltCircleLeft} /></Link>
+            <div className="btn-container">
+                <Link className="back-btn first-btn" to="/play"><FontAwesomeIcon className="back-btn-icon" icon={faArrowAltCircleLeft} /></Link>
+                <FontAwesomeIcon onClick={handleClick} className="back-btn-icon second-btn" icon={faQuestionCircle} />
+            </div>
             <p className={clicked ? "question-move" : "question"}> {props.question(id - 1)} </p>
             {props.img(id - 1) != undefined && <img class={clicked ? "question-img-move" : "question-img"} src={`../imgs/${props.img(id - 1)}.jpg`} alt="question-img" />}
             {clicked && <div><p className="answer"> {props.answer(id - 1)} </p></div>}
-            <button class="show-answer-btn" onClick={handleClick}>Ответ</button>
         </div>
     )
 }
